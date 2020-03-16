b09e2bf5abfd006b23749eb47e34618e1df3a704
Johannes Sundqvist <johannes@sundqvist.io>
2020-03-16 20:03:20 +0100

    remove zimfw install step and fix ln

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env zsh
 
-git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
-
-for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/*; do
-  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-  cat ${template_file} ${user_file}(.N) > ${user_file}.tmp && mv ${user_file}{.tmp,}
-done
-
-source ${ZDOTDIR:-${HOME}}/.zlogin
-
-ln -s ${0:a:h}/bastard.zsh-theme $HOME/.zim/modules/prompt/functions/prompt_bastard_setup
+ln -s ${0:a:h}/bastard.zsh-theme $HOME/.zim/modules/bastard
 sed -i.bak "s/zprompt_theme='.*'/zprompt_theme='bastard'/g" $HOME/.zimrc
 
 zsh
